41dbf2bfd29e8f089137979a67ba48ccebd9a8a0
JulieCass1308 <julie0510@live.fr>
2025-11-25 13:27:09 +0100

    first commit

---
+++ test.js
@@ -0,0 +1,65 @@
+/* commentaire commme en css sur 
+plusieurs lignes 
+*/
+
+// un commentaire sur une ligne
+
+// console.log("Hello world !");
+
+// console.log(`Hello world ! "je suis d'accord !"`);
+
+// Variable
+// let nom = "Toto";
+
+// Array
+// let names = ["Riri", "Fifi", "Loulou"]
+
+// console.log(nom);
+
+// console.log("Salut " + nom);
+
+// console.log("Salut " + names[0]);
+
+// nom = "Tutu";
+
+// console.log(nom);
+
+// const lastName = "Truc";
+
+// console.log(lastName);
+
+// lastName = "Muche";//Error
+
+let age = 18; //type number
+
+console.log(age);
+
+console.log(typeof age);
+
+// if (age > 18) {
+//   console.log("droit de conduire");
+// } 
+// else if (age == 18) {
+//   console.log("anniv");
+// } 
+// else if (age < 18 ) {
+//   console.log("patience !");
+// }
+// else {
+//     console.log("Veuillez entrer un nombre");
+// }
+
+
+if (typeof age !== "number") {
+    console.log(age + "n'est pas un nombre");
+} else {
+    if (age > 18) {
+  console.log("droit de conduire");
+} 
+else if (age == 18) {
+  console.log("anniv");
+} 
+else {
+  console.log("patience !");
+}
+}
